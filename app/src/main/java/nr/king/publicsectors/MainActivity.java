80aea54c314c62dd7b3be2f9af8f7f995578f93b
trojanvbk <kbharath326@gmail.com>
2020-09-06 12:46:28 +0530

    Group 4

---
+++ app/src/main/java/nr/king/publicsectors/MainActivity.java
@@ -0,0 +1,19 @@
+package nr.king.publicsectors;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nr.king.publicsectors.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
